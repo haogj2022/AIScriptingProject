ad91cee4b1a7d5e64433169860b6c86e84b408c4
haogj2022 <106388383+haogj2022@users.noreply.github.com>
2023-10-18 17:55:29 +0700

    pathnode+pathfinding

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
 
 public class Testing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Pathfinding pathfinding;
+
+    private void Start()
     {
-        Grid grid = new Grid(4, 2, 10);
+        pathfinding = new Pathfinding(10, 10);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
+            List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
+
+            if (path != null)
+            {
+                for (int i = 0; i < path.Count - 1; i ++)
+                {
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, new Vector3(path[i+1].x, path[i+1].y) * 10f + Vector3.one * 5f, Color.green, int.MaxValue);
+                }
+            }
+        }
     }
 }
